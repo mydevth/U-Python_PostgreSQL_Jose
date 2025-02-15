b4f2e60b1107704785426c179f74087121f22b32
mydevth <mydevth@chonburi.biz>
2025-02-16 02:15:07 +0700

    add

---
+++ 01_variables/code.py
@@ -0,0 +1,12 @@
+# -- Number --
+x = 15
+price = 9.99
+discount = 0.2
+result = price * (1 - discount)
+print(result)  # 7.992
+
+#---- String --
+name = "Rolf"
+name = "Rolf"
+print(name)  # Rolf
+print(name * 3)  # RolfRolf
