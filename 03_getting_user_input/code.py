1627bf8cf0fad8ee4cf7850d415359ce05f926f7
mydevth <mydevth@chonburi.biz>
2025-02-16 23:50:14 +0700

    day2

--- 03_getting_user_input/code.py
+++ 03_getting_user_input/code.py
@@ -5,4 +5,4 @@ print(name)
 size_input = input("How bit is your house (in square feer): ")
 square_feet = int(size_input)
 square_metres = square_feet / 10.8
-print(f"{square_feet} square feet is {square_metres} square metres.")
+print(f"{square_feet} square feet is {square_metres:.2f} square metres.")
---
+++ 04_first_python_app/code.py
@@ -0,0 +1,4 @@
+user_age=int(input("Enter your age: "))
+months = user_age * 12
+
+print(f"{user_age} years is equal to {months} months.")  # 4863.434
