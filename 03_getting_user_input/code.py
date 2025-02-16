534336352a975266d64b0500a640d0832a99d2c2
mydevth <mydevth@chonburi.biz>
2025-02-16 21:02:04 +0700

    03

---
+++ 03_getting_user_input/code.py
@@ -0,0 +1,8 @@
+name=input("Enter your name: ")
+print(name)
+
+# -- Mathematics on user input --
+size_input = input("How bit is your house (in square feer): ")
+square_feet = int(size_input)
+square_metres = square_feet / 10.8
+print(f"{square_feet} square feet is {square_metres} square metres.")
